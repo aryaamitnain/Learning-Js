d1dd1ee8c02e77c85ee033070e199b2353332cb6
Amit Nain <159742510+ChAmitNain@users.noreply.github.com>
2024-02-16 06:08:07 +0000

    comparison data types

---
+++ 01_basics/comparisonDataType.js
@@ -0,0 +1,15 @@
+// console.log(2 < 5);
+// console.log(2 > 5);
+// console.log(2 == 5);
+// console.log(2 <= 5);
+// console.log(2 >= 5);
+// console.log(2 != 5);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null<0);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
