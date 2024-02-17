3fd92e99882f9a35ceeae0575b2bd7dae8a397c9
Amit Nain <159742510+ChAmitNain@users.noreply.github.com>
2024-02-17 12:14:58 +0000

    string

--- 01_basics/04_string.js
+++ 01_basics/04_string.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "Amit"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('amit-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,23 +13,23 @@ const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   amit nain   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+// const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+// console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
