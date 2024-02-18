eb9af51b15826c2a17f926fe39f8d0fa72e632ae
Amit Nain <159742510+ChAmitNain@users.noreply.github.com>
2024-02-18 17:17:58 +0000

    Numbers and Maths

---
+++ 01_basics/05_Nums_and_maths.js
@@ -0,0 +1,44 @@
+const score = 400;
+// console.log(typeof(score));
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const numOne = 23.45786;
+
+// console.log(numOne.toFixed(2));
+
+// console.log(numOne.toPrecision(3));
+
+const othernumber = 10000000;
+
+// console.log(othernumber.toLocaleString('en-In'));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const NumTwo = 5.6;
+
+// console.log(Math);
+// console.log(Math.abs(-56));
+// console.log(Math.round(3.7));
+// console.log(Math.ceil(NumTwo));
+// console.log(Math.floor(NumTwo));
+
+const tosqrt = 144;
+
+// console.log(Math.sqrt(tosqrt));
+// console.log(Math.min(3, 5, 14, 59, 7));
+// console.log(Math.max(3, 5, 14, 59, 7));
+
+console.log(Math.random()*10);
+console.log(Math.random()*10 +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10;
+const max = 50;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
