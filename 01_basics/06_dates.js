69d87369a754c5e568618f2bd69913e7f0e40a86
Amit Nain <159742510+ChAmitNain@users.noreply.github.com>
2024-02-19 14:51:53 +0000

    dates

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate = new Date(2022, 13, 23);
+
+// let myCreatedDate = new Date(2022, 3, 4, 5, 3);
+// let myCreatedDate = new Date("2023-01-13");
+let myCreatedDate = new Date("01-04-2024");
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
