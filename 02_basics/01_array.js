f2beeab72843e580e7ff299c2233a0636cf6939e
Amit Nain <159742510+ChAmitNain@users.noreply.github.com>
2024-02-21 17:53:26 +0000

    started array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,34 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const names = ["amit", "rohit", "gaurav"]
+
+// console.log(myArray[2]);
+// console.log(names[2]);
+
+// methods
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+
+// myArray.unshift(5);
+// myArray.shift();
+
+// console.log(myArray.includes(7));
+// console.log(myArray.indexOf(4));
+
+let newArray = myArray.join()
+
+// console.log(newArray);
+// console.log(typeof(newArray));
+
+console.log("A", myArray);
+
+const myN1 = myArray.slice(0,4)
+console.log(myN1);
+console.log("B", myArray);
+
+const myN2 = myArray.splice(0,4)
+console.log(myN2);
+console.log('c', myArray);
