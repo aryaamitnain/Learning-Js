34a9231fcc33f6a8ade9a47a8b9077f500c6d08e
Amit Nain <aryaamitnain08@gmail.com>
2024-05-04 05:47:16 +0000

    passing objects in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,20 @@ function loginUserMessage(username = "Amit"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Amit Nain"))
-console.log(loginUserMessage("Rahul"))
+// console.log(loginUserMessage("Rahul"))
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1
+}
+// console.log(calculateCartPrice(200, 300, 500, 800))
+
+const user = {
+  Name : "Amit",
+  Price : 500
+}
+
+function handleObject(anyobject){
+  console.log(`username is ${anyobject.Name} and price is ${anyobject.Price}`);
+}
+
+handleObject(user)
